3bf0abafa83d0f35344eb3437c472c91d3cc9c0a
igoreta <ivoronline@gmail.com>
2021-03-18 11:35:12 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_log_logback_config_xml_db_custom/SpringbootLogLogbackConfigXmlDbCustomApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_log_logback_config_xml_db_custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLogLogbackConfigXmlDbCustomApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootLogLogbackConfigXmlDbCustomApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_log_logback_config_xml_db_custom/SpringbootLogLogbackConfigXmlDbCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_log_logback_config_xml_db_custom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLogLogbackConfigXmlDbCustomApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
